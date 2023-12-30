5475833ba17c889f9fb3c52da6ef522cfb4647a1
nghieptx <nghiepradeon@gmail.com>
2023-12-31 01:20:02 +0700

    config env

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const app = require("./src/app");
-const PORT = 3055;
+const {
+  app: { port },
+} = require("./src/configs/config.mongodb");
+
+const PORT = port || 3055;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-process.on("SIGINT", () => {
-  server.close(() => {
-    console.log(`Exit server Express`);
-  });
-});
+// process.on("SIGINT", () => {
+//   server.close(() => {
+//     console.log(`Exit server Express`);
+//   });
+// });
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -2,7 +2,10 @@
 
 const mongoose = require("mongoose");
 const { countConnect } = require("../helpers/check.connect");
-const connectString = `mongodb://localhost:27017/shopDEV`;
+const {
+  db: { host, port, name },
+} = require("../configs/config.mongodb");
+const connectString = `mongodb://${host}:${port}/${name}`;
 
 class Database {
   constructor() {
