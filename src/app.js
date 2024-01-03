dbb907a9658af34492fe2a7b2a2d59c4b86c6598
nghieptx <nghiepradeon@gmail.com>
2024-01-04 01:03:49 +0700

    success response

--- src/app.js
+++ src/app.js
@@ -24,12 +24,6 @@ app.use("/", require("./routers"));
 
 // handling error
 
-app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
-});
-
 app.use((error, req, res, next) => {
   const statusCode = error.status || 500;
   res.status(statusCode).json({
--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const { CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
     console.log(`[P]::signUp`, req.body);
-    return res.status(201).json(await AccessService.signUp(req.body));
+    new CREATED({
+      message: "Successfully signed up",
+      metadata: await AccessService.signUp(req.body),
+    }).send(res);
   };
 }
 
